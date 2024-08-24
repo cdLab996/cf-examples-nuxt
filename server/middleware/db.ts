dae7d51084afef0095bb3c7ff8f091c487fc7ebd
wudi <wuchendi96@gmail.com>
2024-08-24 14:02:44 +0800

    fix(server): Log Error When D1 Database is Missing
    The middleware now logs an error if the D1 database is not found in the Cloudflare environment, rather than silently returning a default database. This change aids in better diagnosing database connection issues.

--- server/middleware/db.ts
+++ server/middleware/db.ts
@@ -8,6 +8,7 @@ import type { H3Event, EventHandlerRequest } from 'h3'
 import { drizzle as drizzleSqlite } from 'drizzle-orm/libsql'
 import { drizzle as drizzleD1 } from 'drizzle-orm/d1'
 import * as schema from '~~/db/schema'
+import logger from '../utils/logger'
 
 function initializeDrizzle(event: H3Event<EventHandlerRequest>) {
   const { dbType } = useRuntimeConfig()
@@ -18,11 +19,16 @@ function initializeDrizzle(event: H3Event<EventHandlerRequest>) {
       return drizzleSqlite(db, { schema })
     }
     case 'cf': {
-      const { DB } = event.context.cloudflare.env
+      const { DB = '' } = event.context.cloudflare?.env || {}
+      if (!DB) {
+        logger.error('D1 database not found')
+        return useDatabase()
+      }
       return drizzleD1(DB, { schema })
     }
     default: {
-      throw new Error(`Unsupported DB type: ${dbType}`)
+      logger.error(`Unsupported DB type: ${dbType}`)
+      return useDatabase()
     }
   }
 }
